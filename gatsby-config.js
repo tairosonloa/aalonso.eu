02dad8885c9bbe9b653aadc34b34a51d5dff3529
Aitor Alonso <aalonso@aalonso.eu>
2019-11-25 22:49:08 +0100

    Added Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-153375682-1`,
       },
     },
     `gatsby-plugin-feed`,
