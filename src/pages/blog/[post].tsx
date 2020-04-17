4bf9a535260c2b9ba7f9cc581a5dd1070003a5e5
Aitor Alonso <tairosonloa@gmail.com>
2020-04-17 02:07:41 +0200

    chore: airbnb eslint and prettier config. Remove travis

--- src/pages/blog/[post].tsx
+++ src/pages/blog/[post].tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement } from 'react'
 
 export default function PostTemplate(props: any): ReactElement {
-  return <div>{props.post}</div>
+  const { post } = props
+  return <div>{post}</div>
 }
 PostTemplate.getInitialProps = async (context: any): Promise<any> => {
   const { post } = context.query
